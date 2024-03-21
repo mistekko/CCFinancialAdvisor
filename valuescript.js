af5cb2d5c9f1560ace43460b60b4ee693f40e86d
mistekko <steamlie@protonmail.com>
2024-03-20 20:51:41 -0500

    efficiency works fully

--- valuescript.js
+++ valuescript.js
@@ -1,17 +1,29 @@
 var table_contents = [];
-for (var i = 0; i <= 10; i++) {
-    //print out the product name, its price, and its value
-    //if an item has not been unlocked, break (their class will be "product unlocked disabled
+for (var i = 0; i <= 19; i++) {
+    
     var name  = document.getElementById(`productName${i}`).innerHTML;
-    if (name.substring(0,1) =='<' ) {
+    if (name.substring(0,1) =='<' ) { //sometimes (but not always!!) productName ID contains an entire div in its innerHTML. What! None of that!
       name = name.split('>')[1].split('<')[0];
     }
-    var price_raw = document.getElementById(`productPrice${i}`).innerHTML.split(' ');
-    var price_digits = price_raw[0];
-    var price_order  = ' ' + price_raw[1];
-    var price_real = price_digits * Math.pow(10, (3 * (formatLong.indexOf(price_order) + 1)));
 
-    table_contents.push({name:`${name}`,price:`${price_real}`});
+    var price_raw = document.getElementById(`productPrice${i}`).innerHTML
+    var price_digits = price_raw.split(' ')[0];
+    var price_order  = ' ' + price_raw.split(' ')[1]; //millions? billions?
+    var price_real = price_digits * Math.pow(10, (3 * (formatLong.indexOf(price_order) + 1))); //computers are much better at using 1000 than 'a thousand'
+
+    var quantity = document.getElementById(`productOwned${i}`).innerHTML;
+  
+    var cps = Game.cookiesPsByType[name] * Game.globalCpsMult / quantity; 
+  		
+  
+    if (cps > 0) {
+	var efficiency = Beautify(price_real / cps);
+	cps = Beautify(cps,3);
+	
+      
+    	table_contents.push({name:`${name}`,price:`${price_raw}`,cps:`${cps}`,efficiency:`${efficiency}`});
+    } else break; //this improves the efficiency slightly, but causes bugs for any psychos who completely skip any building.
+    
 
 }
 

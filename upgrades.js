1083ebd2f78f4ec50108937684a742bd1f3ff34f
mistekko <steamlie@protonmail.com>
2024-03-23 14:53:30 -0500

    scripts are now functions, meaning they can more easily be invoked

--- upgrades.js
+++ upgrades.js
@@ -1,53 +1,53 @@
-var upgrade_arr = [];
-var unformatted_arr = [];
-var append = false;
+const upgrades = function () {
+    var upgrade_arr = [];
+    var unformatted_arr = [];
+    var append = false;
 
-for (var i = 0; i < Game.UpgradesInStore.length; i++) {
-    upgrade = Game.UpgradesInStore[i]
-    var name  = upgrade.name; 
-    var price = upgrade.basePrice;
+    for (var i = 0; i < Game.UpgradesInStore.length; i++) {
+	upgrade = Game.UpgradesInStore[i]
+	var name  = upgrade.name; 
+	var price = upgrade.basePrice;
 
-    //if cookie: pool prop == 'cookie'
-    if (upgrade.pool=='cookie') {
-	var target = 'cookie production';
-	var mult = upgrade.power / 100;
-	var cps_current = Game.cookiesPs;
-	append = true;
-	var cps_gain = cps_current * mult;
-	var cpcps = price / cps_gain
-    }
-    
-    else if (upgrade.buildingTie != 0) { //if it's a building upgrade
-	var building = Game.UpgradesInStore[i].buildingTie;
-	var target = building.name
-	var cps_gain = Game.cookiesPsByType[building.name] * Game.globalCpsMult;
-	append = true;
-    }
+	//if cookie: pool prop == 'cookie'
+	if (upgrade.pool=='cookie') {
+	    var target = 'cookie production';
+	    var mult = upgrade.power / 100;
+	    var cps_current = Game.cookiesPs;
+	    append = true;
+	    var cps_gain = cps_current * mult;
+	    var cpcps = price / cps_gain
+	}
+	
+	else if (upgrade.buildingTie != 0) { //if it's a building upgrade
+	    var building = Game.UpgradesInStore[i].buildingTie;
+	    var target = building.name
+	    var cps_gain = Game.cookiesPsByType[building.name] * Game.globalCpsMult;
+	    append = true;
+	}
 	
 
-    if (append) {
-	var cpcps = price / cps_gain;
-	upgrade_arr.push({name:`${name}`,target:`${target}`,price:`${Beautify(price)}`,cps:`${Beautify(cps_gain)}`,cpcps:`${Beautify(cpcps)}`});
-	unformatted_arr.push({name:`${name}`,target:`${target}`,price:price,cps:`${cps_gain}`,cpcps:cpcps});
-	append = false;
+	if (append) {
+	    var cpcps = price / cps_gain;
+	    upgrade_arr.push({name:`${name}`,target:`${target}`,price:`${Beautify(price)}`,cps:`${Beautify(cps_gain)}`,cpcps:`${Beautify(cpcps)}`});
+	    unformatted_arr.push({name:`${name}`,target:`${target}`,price:price,cps:`${cps_gain}`,cpcps:cpcps});
+	    append = false;
+	}
+	
+	
+    }
+    console.table(upgrade_arr);
+
+    var best_cpcps = Number.MAX_SAFE_INTEGER;
+    var best_name;
+    var best_price;
+    for (i in unformatted_arr) {
+	if (unformatted_arr[i].cpcps < best_cpcps) {
+	    best_cpcps = unformatted_arr[i].cpcps;
+	    best_name  = unformatted_arr[i].name;
+	    best_price = unformatted_arr[i].price;
+	}
     }
     
-    
+    console.log(`Best option: ${best_name} w/ ${Beautify(best_cpcps)} (${Beautify(best_price)})`);
 }
-console.table(upgrade_arr);
-
-var best_cpcps = Number.MAX_SAFE_INTEGER;
-var best_name;
-var best_price;
-for (i in unformatted_arr) {
-    if (unformatted_arr[i].cpcps < best_cpcps) {
-      best_cpcps = unformatted_arr[i].cpcps;
-      best_name  = unformatted_arr[i].name;
-      best_price = unformatted_arr[i].price;
-  }
-}
- 
-console.log(`Best option: ${best_name} w/ ${Beautify(best_cpcps)} (${Beautify(Number(best_price))})`);
-
-    
     
--- valuescript.js
+++ valuescript.js
@@ -1,49 +1,48 @@
-var table_contents = []; //to store human-readable data
-var cpcps_array = []; //to store unformated data
-for (var i = 0; i <= 19; i++) {
-    
-    var name  = document.getElementById(`productName${i}`).innerHTML;
-    if (name.substring(0,1) =='<' ) { //sometimes (but not always!!) productName ID contains an entire div in its innerHTML. What! None of that!
-      name = name.split('>')[1].split('<')[0];
+const bV = function () {
+    var table_contents = []; //to store human-readable data
+    var cpcps_array = []; //to store unformated data
+    for (var i = 0; i <= 19; i++) {
+	
+	var name  = document.getElementById(`productName${i}`).innerHTML;
+	if (name.substring(0,1) =='<' ) { //sometimes (but not always!!) productName ID contains an entire div in its innerHTML. What! None of that!
+	    name = name.split('>')[1].split('<')[0];
+	}
+
+	var price_raw = document.getElementById(`productPrice${i}`).innerHTML
+	var price_digits = price_raw.split(' ')[0];
+	var price_order  = ' ' + price_raw.split(' ')[1]; //millions? billions?
+	var price_real = price_digits * Math.pow(10, (3 * (formatLong.indexOf(price_order) + 1))); //computers are much better at using 1000 than 'a thousand'
+
+	var quantity = document.getElementById(`productOwned${i}`).innerHTML;
+	
+	var cps = Game.cookiesPsByType[name] * Game.globalCpsMult / quantity; 
+  	
+	
+	if (cps > 0) {
+	    var cpcps = price_real / cps;
+	    cpcps_array.push(cpcps);
+	    cpcps = Beautify(cpcps,3);
+	    cps = Beautify(cps,3);
+	    
+	    
+    	    table_contents.push({name:`${name}`,price:`${price_raw}`,cps:`${cps}`,cpcps:`${cpcps}`});
+	} else break; //this improves the efficiency slightly, but causes bugs for any psychos who completely skip any building.
     }
 
-    var price_raw = document.getElementById(`productPrice${i}`).innerHTML
-    var price_digits = price_raw.split(' ')[0];
-    var price_order  = ' ' + price_raw.split(' ')[1]; //millions? billions?
-    var price_real = price_digits * Math.pow(10, (3 * (formatLong.indexOf(price_order) + 1))); //computers are much better at using 1000 than 'a thousand'
+    console.table(table_contents);
 
-    var quantity = document.getElementById(`productOwned${i}`).innerHTML;
-  
-    var cps = Game.cookiesPsByType[name] * Game.globalCpsMult / quantity; 
-  		
-  
-    if (cps > 0) {
-	var cpcps = price_real / cps;
-	cpcps_array.push(cpcps);
-	cpcps = Beautify(cpcps,3);
-	cps = Beautify(cps,3);
-	
-      
-    	table_contents.push({name:`${name}`,price:`${price_raw}`,cps:`${cps}`,cpcps:`${cpcps}`});
-    } else break; //this improves the efficiency slightly, but causes bugs for any psychos who completely skip any building.
+
+    //print what purchase would be the best so that the user doesn't have to go through the entire table, potentially making mistakes
+    var best_cpcps = Number.MAX_SAFE_INTEGER;
+    var best_name;
+    var best_price;
+    for (i in table_contents) {
+	if (cpcps_array[i] < best_cpcps) {
+	    best_cpcps = cpcps_array[i];
+	    best_name  = table_contents[i].name;
+	    best_price = table_contents[i].price;
+	}
+    }
+
+    console.log(`Best option: ${best_name} w/ ${Beautify(best_cpcps)} (${best_price})`);
 }
-
-console.table(table_contents);
-
-
-//print what purchase would be the best so that the user doesn't have to go through the entire table, potentially making mistakes
-var best_cpcps = Number.MAX_SAFE_INTEGER;
-var best_name;
-var best_price;
-for (i in table_contents) {
-  if (cpcps_array[i] < best_cpcps) {
-      best_cpcps = cpcps_array[i];
-      best_name  = table_contents[i].name;
-      best_price = table_contents[i].price;
-  }
-}
-
-console.log(`Best option: ${best_name} w/ ${Beautify(best_cpcps)} (${best_price})`);
-
-    
-    

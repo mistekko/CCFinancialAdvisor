cc53a84ec284f08e5a686265f04ac05ea0421c88
mistekko <steamlie@protonmail.com>
2024-03-30 09:58:55 -0500

    added proper analysis of grandma synergy upgrades

---
+++ grandma_synergies.js
@@ -0,0 +1,21 @@
+//print the following:
+//percentage buffs for each buliding type (raw synergy)
+//production by that building type
+//total cookies buffed by grandmas for that building type
+//cookies added per grandma for that building type
+
+//idea for synergy data gathering: use the tooltip function of each the game object (building) to grab the raw html and then extract data from that; better idea: DON'T.
+
+
+//building for a particular grandma synergy index 'n': Game.Upgrades[Game.GrandmaSynergies[4]].buildingTie
+
+
+//Grandma synergy upgrades lie in the upgrade ID range of 57-63
+//synergy upgrade for a particular building: 55 + building ID
+//	-they have a building tie to the building they synergise with 
+
+
+
+*/
+
+

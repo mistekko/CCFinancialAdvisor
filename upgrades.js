b2ee1d0b12985ce9c770cb1986453cdc9eeb2498
mistekko <steamlie@protonmail.com>
2024-04-12 07:24:12 -0500

    kittens appear to be processed correctly now

--- upgrades.js
+++ upgrades.js
@@ -1,4 +1,3 @@
-console.log("uhhh")
 var get_best_upgrades = function () {
     //get pool for kitten upgrades: Check if the first word is kitten. q
     var upgrade_table_contents = [];
@@ -17,19 +16,17 @@ var get_best_upgrades = function () {
     var specific_kitten_multiplier = [0.1,0.125,0.15,0.175,0.2,0.2,0.2,0.2,0.2,0.175,0.15,0.125,0.115,0.11,0.105,0.1,0.05];
 
     var best_upgrade = {cpcps:Number.MAX_SAFE_INTEGER,name:"",price:0};
-
-    console.log("it sould print run on the next line!")
     
     for (var i = 0; i < Game.UpgradesInStore.length; i++) { //bug: this is executing... inifinitely... !
 	upgrade = Game.UpgradesInStore[i];
 	name    = upgrade.name; 
 	price   = upgrade.basePrice;
 
-	console.log("run!")
-
 	if (Game.UpgradesByPool['kitten'].indexOf(upgrade) > 0) { //if the upgrade is in the list of upgrades that are kitten upgrades because Orteil couldn't be arsed to actually use the pool prop consistently	    
 	    //the following code was taken directly from cookie clicker source code
 	    //start stolen code //it would really be nice if milkMult (here milk_multiplier) was accessible from main.js so I wouldn't have to steal code and blaot this program!
+	    console.log("kitty found")
+	    
 	    mult = 1; var milk_multiplier=1;
 
 	    if (Game.Has('Santa\'s milk and cookies')) milk_multiplier*=1.05;
@@ -43,14 +40,18 @@ var get_best_upgrades = function () {
 	    }
 	    milk_multiplier*=Game.eff('milk');
 	    //end stolen code
-	    
-	    //Orteil is a frenchman (madman) so we have to gather all the kitten-specific multipliers (the third value in the parethesised expression after each "catMult*=") into an array in order to algorithmically adress them.
-	    for (var i = 0; i < specific_kitten_multiplier.length; i++) 
-		if (Game.Has(Game.UpgradesByPool['kitten'][i].name)) 
-		    mult *= (1 + Game.milkProgress * specific_kitten_multiplier[i] * milk_multiplier);
-	    cps_gain = current_total_cps / Game.cookiesMultByType['kittens'] * mult - Game.cookiesPs;
-	}
 
+	    milk_multiplier *= Game.milkProgress;
+
+	    let a = 1 
+	    for (var j = 0; j < specific_kitten_multiplier.length; j++) 
+		if (Game.Has(Game.UpgradesByPool['kitten'][j].name)) 
+		    mult *= (1 + specific_kitten_multiplier[j] * milk_multiplier);
+	    mult *= (1 + specific_kitten_multiplier[Game.UpgradesByPool['kitten'].indexOf(upgrade)] * milk_multiplier);
+	    cps_gain = current_total_cps / Game.cookiesMultByType['kittens'] * mult - Game.cookiesPs;
+	    append = true
+	}
+	
 	else if (Game.GrandmaSynergies.indexOf(name) > -1) { //if it's a grandma synergy
 	    building = upgrade.buildingTie;
 	    granny_quantity = Game.Objects["Grandma"].amount;
@@ -75,17 +76,17 @@ var get_best_upgrades = function () {
 	    append = true;
 	}
 
-	
+	    
 	if (append) { // we need this data for ever upgrade! 
 	    cpcps = price / cps_gain;
 	    upgrade_table_contents.push({name:`${name}`,target:`${target}`,price:`${Beautify(price)}`,cps:`${Beautify(cps_gain)}`,cpcps:`${Beautify(cpcps)}`});
 	    append = false;
 	}
-
+	
 
 	if (cpcps < best_upgrade.cpcps) {
 	    best_upgrade = {cpcps:cpcps,name:name,price:price};
-	}	
+	}
     }
 
     
